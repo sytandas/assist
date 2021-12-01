1415726535718797c13a805d087015867cb90aff
sayantan3 <sayantand975@gmail.com>
2021-12-01 08:46:35 +0530

    intial commit

---
+++ chat.js
@@ -0,0 +1,7 @@
+//chat.js file connect with the chat.html and interect with the user input - and reply accordingly 
+//intially we make it simple one question to another reply format 
+//future TODO: machine learning algortihm to predict and implement reply(s)
+
+function(reply){
+
+}
