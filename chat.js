c13404e8da6f24f2f26afe0fa1a4aa24dffc9d0f
sayantan3 <sayantand975@gmail.com>
2022-01-03 23:15:16 +0530

    update

--- chat.js
+++ chat.js
@@ -1,22 +1,19 @@
 
 console.log('connected');
 
+// TODO - every time it's open create a file[date/time of creation] - enter i/p data to the file as "Human: ..." 
+
+/*
 document.getElementById('inputfile').addEventListener('change', function() {
   var fr = new FileReader();
   fr.onload=function(){
     document.getElementById('output').textContent=fr.result;
-  }
+  } 
   fr.readAsText(this.files[0]);
 })
 
+//var pg = require('pg'); // connect postgredb
 
-
-
-
-
-
-
-/*
 import * as fs from 'fs'; // importing function for file handling - fs
 
 //create file - at the time of page load/refresh 
@@ -31,41 +28,42 @@ window.onload = function file_create(){
   });
 }
 
-//read file - scope ~ lexical analysis
+*/
 
-function file_read(){
-}
-
-//writng to the file 
-
-function file_write(){
+function saveTextContent() {
+  var textToWrite = document.getElementById('chatInput').value;
+  document.getElementById('saveinput').value = txt;
+  document.forms['aForms'].submit();
+  var fileNameToSaveAs = 'output/file.txt';
   
 }
+  /*
 
-//reply function
+  var downloadLink = document.createElement('a');
+  downloadLink.download = fileNameToSaveAs;
+  downloadLink.innterHTML = 'Download File';
 
-function reply(){
-  
+  if (window.webkitURL != null){
+    // chrome allows the link to be clicked without actually adding it to the DOM
+
+  downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+  } else {
+    // Firefox requires the link to be added to the DOM before it can be clicked
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+  }
+
+  downloadLink.click();
 }
 
-//lexical analysis 
+var button = document.getElementById('send');
+button.addEventListener('click', saveTextContent);
 
-function check(){
-
-}
-
-//terminating function
-
-function terminate(){
-
-}
-
-function show(){
-  var fs = require('fs');
-  fs.openFile('/output/file.txt', function (err){
-    if (err) throw err;
-    console.log('Open');
-  });
+function destroyClickedElement(event) {
+  document.body.removeChild(event.target);
 }
 */
+
 
