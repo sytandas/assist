5f5f9b241588ec3263080fdd1b1843e9f8dae783
sayantan3 <sayantand975@gmail.com>
2022-02-07 11:43:42 +0530

    retrieval

--- chat.js
+++ chat.js
@@ -1,3 +1,23 @@
 console.log('connected');
 
+// function for refresing page every 5 sec
 
+setTimeout(function(){
+  window.location.href = '/Users/sayantandas/fun/assist/chat.html';
+}, 5000);
+
+// write into the file
+
+function WriteToFile(){
+  var fso = new ActiveXObject("Scripting.FileSystemObject");
+  var test = document.getElementById("textArea").innerText;
+  var s = fso.CreateTextFile("output/"+file+".txt", true);
+  s.WriteLine(text);
+  s.close();
+}
+
+// button action to write text area document to the file 
+
+function saveTextContent(){
+
+}
