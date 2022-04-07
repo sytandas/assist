0525d0925590cc5f21f69af16a7b963c3cf13e84
sayantan3 <sayantand975@gmail.com>
2022-04-07 13:27:56 +0530

    new

--- chat.js
+++ chat.js
@@ -1,13 +1,11 @@
 console.log('connected');
 
-// function for refresing page every 5 sec
-
 setTimeout(function(){
   window.location.href = '/Users/sayantandas/fun/assist/chat.html';
 }, 50000); //TODO right not it set to 50 sec 
 
 // write into the file
-
+/*
 function WriteToFile(){
   var fso = new ActiveXObject("Scripting.FileSystemObject");
   var test = document.getElementById("textArea").innerText;
@@ -15,9 +13,17 @@ function WriteToFile(){
   s.WriteLine(text);
   s.close();
 }
-
+*/
 // button action to write text area document to the file 
 
 function saveTextContent(){
-      
+  const fs = require('fs');
+  var store = document.getElementsById("textArea").innerHTML;
+  var data = document.getElementById("textArea").innerText;
+  fs.writeFile("output/"+file+".txt", data, (err) => {
+    if (err)
+      throw err;
+  })
 }
+
+
