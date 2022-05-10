b1d6f187e1c15d35c4aeda786cf7f663ddae4c04
sayantan3 <sayantand975@gmail.com>
2022-05-10 21:36:24 +0530

    update

--- chat.js
+++ chat.js
@@ -4,18 +4,6 @@ setTimeout(function(){
   window.location.href = '/Users/sayantandas/fun/assist/index.html';
 }, 50000); //TODO right not it set to 50 sec 
 
-// write into the file
-/*
-function WriteToFile(){
-  var fso = new ActiveXObject("Scripting.FileSystemObject");
-  var test = document.getElementById("textArea").innerText;
-  var s = fso.CreateTextFile("output/"+file+".txt", true);
-  s.WriteLine(text);
-  s.close();
-}
-*/
-// button action to write text area document to the file 
-
 function saveTextContent(){
   const fs = require('fs');
   var store = document.getElementsById("textArea").innerHTML;
@@ -24,6 +12,10 @@ function saveTextContent(){
     if (err)
       throw err;
   })
+}
+
+function refreshPage(){
+  reset.onclick = window.location.reload();
 }
 
 
