3f38fec31fad4abe05b304efa406eff852a23cb2
sayantan3 <sayantand975@gmail.com>
2023-05-08 20:55:32 +0530

    chat gpt api

---
+++ apt-gpt.py
@@ -0,0 +1,18 @@
+import os
+import openai
+
+openai.api_key = ("<your open-api api key>") # reaplce <> with your own open-ai api key
+ 
+while True:
+  question = input("\033[34mwhat is your question?\n\033[0m")
+  if question.lower() == "exit":
+    print("\033[31mEXIT\033[0m")
+    break
+	
+  completion = openai.ChatCompletion.create(
+	model="gpt-3.5-turbo",
+        messages=[{"role": "system", "content": "You are helpful assistant. Answer the given question."},
+              {"role": "user", "content": question}
+              ]
+         )
+  print("\033[32m" + completion.choices[0].message.content + "\n")
